f6924831ef66af8b68dad138c6e2146cde24011e
JoaoSirico <150676023+JoaoSirico@users.noreply.github.com>
2024-05-07 18:30:19 -0300

    JS
    Adição de js

--- js.js
+++ js.js
@@ -1,8 +1,25 @@
 const inserir = document.querySelector("#inserir-dados");
 
-fetch('https://jsonplaceholder.typicode.com/todos/1')
-      .then(response => response.json())
-      .then(json => console.log(json))
-      .then(inserir.append(json));
+fetch('https://jsonplaceholder.typicode.com/todos')
+      .then(response => {
+            if (!response.ok) {
+                  throw new Error('Erro ao obter os dados');
+            }
+            return response.json();
+      })
+      .then(data => {
+            data.forEach(item => {
+                  const p = document.createElement('p');
+                  p.textContent = `ID: ${item.id}`;
+                  //inserir.appendChild(p);
+            });
+      })
+      .catch(error => {
+            console.error("ERRO: " + error);
+      });
 
-
+function pesquisaId(){
+      const pesquisa = document.querySelector("#pesquisar-teste");
+      const conteudo = pesquisa.value;
+      console.log(conteudo);
+}
